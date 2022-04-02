4dd5df56bcc1cd1e88e8fcc3e719d4ac395a4886
vasanthi-dev <vasanthi2dev@gmail.com>
2022-04-02 14:17:06 -0400

    ami creation

--- ami.tf
+++ ami.tf
@@ -1,4 +1,5 @@
-resource "aws_ami_from_instance" "example" {
+resource "aws_ami_from_instance" "ami" {
+  depends_on = [null_resource.app-deploy]
   name               = "${var.COMPONENT}-${var.APP_VERSION}"
   source_instance_id = aws_instance.ami.id
   tags = {
